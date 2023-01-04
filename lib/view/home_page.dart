0cb21f223dce66b2c46b3f2e5bd59d42544bd46f
isshort <namatullahwahidi@gmail.com>
2023-01-04 12:45:32 +0600

    splash image

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    FlutterNativeSplash.remove();
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('HomePage Title')),
+      body: Container(
+        child: Image.asset('assets/splash12.png'),
+      ),
     );
   }
 }
